4603050745b1417deddd30190a4a85405ca17ec0
ayuubka <ayuubibraahim7474@gmail.com>
2025-07-28 15:26:22 +0300

    change the hero text

---
+++ main.js
@@ -0,0 +1,20 @@
+ const whatsApp = document.getElementById("whatsapp");
+
+
+// whatsApp.addEventListener('click', (event)=>{
+//     // whatsApp.style.backgroundColor="green"
+//     if(whatsApp.event === event){
+//         whatsApp.style.backgroundColor = "green";
+//         return;
+//     }else if(whatsApp.event !== event){
+//         whatsApp.style.backgroundColor = "blue"
+//     }
+// })
+
+// whatsApp.addEventListener('mouseover', (event) => {
+//   if (event.currentTarget === whatsApp) {
+//     whatsApp.style.backgroundColor = 'green';
+//   } else if(event.currentTarget !== whatsApp){
+//     whatsApp.style.backgroundColor = 'blue';
+//   }
+// });
